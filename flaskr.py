b163e6044bf55e72fa0cd5d5410c0ed97a771702
sedevc <info@devc.se>
2015-03-04 23:23:02 +0100

    added root route and "add" route

--- flaskr.py
+++ flaskr.py
@@ -37,5 +37,21 @@ def teardown_request(exception):
         db.close()
 # ******************************
 
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
 if __name__ == '__main__':
     app.run()
